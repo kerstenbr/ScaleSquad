2fb4dfbcbbcd70658df5b5b0acfc40cdf7cc7832
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2023-08-21 08:57:04 -0300

    merge main + bruno

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function($) {
+	/*------------------
+		Background Set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+})(jQuery);
+
